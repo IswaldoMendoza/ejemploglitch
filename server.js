3a92963e71eefadca1ed78b6b61e68b1c6645c02
Iswaldo Mendoza <almacendeiswaldo@gmail.com>
2022-11-30 13:44:58 -0300

    Practica

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 8080
+
+let visitas = 0
+
+
+app.get('/', (req, res) => {
+    res.send(`<h1 style='color:blue'>Hola Mundo</h1>`)
+})
+
+
+app.get('/visitas', (req, res) => {
+    visitas += 1
+    res.send(`La cantidad de visitas es: ${visitas}`)
+})
+
+app.get('/fyh', (req, res) => {
+    const date = new Date
+    res.send(date.toLocaleString())
+})
+
+// Configuramos el puerto
+const server = app.listen(PORT, () => {
+    console.log(`servidor express escuchando en el pto: ${PORT}`);
+}) 
+
+// Escuchamos un evento de error
+server.on('error', error => console.log(`Error en servidor ${error}`))
+
